e5a8529af58dd19d803512d97cc5a174e52d2454
Vestergaard32 <vestergaard32@gmail.com>
2016-03-07 13:30:40 +0100

    Updated files for PSoC0

---
+++ Software/I2C Net/I2C Net/I2C PSoC Net.cydsn/codegentemp/I2C PSoC Net.ctl
@@ -0,0 +1,9 @@
+-- ======================================================================
+-- I2C PSoC Net.ctl generated from I2C PSoC Net
+-- 03/07/2016 at 13:11
+-- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
+-- ======================================================================
+
+-- M0S8 Clock Editor
+-- Directives Editor
+-- Analog Device Editor
--- Software/I2C Net/I2C Net/I2C PSoC Net.cydsn/main.c
+++ Software/I2C Net/I2C Net/I2C PSoC Net.cydsn/main.c
@@ -10,16 +10,29 @@
  * ========================================
 */
 #include <project.h>
+#define SCB_I2C_WRITE_XFER_MODE 0
+#define SCB_I2C_READ_XFER_MODE 1
+
+//Defines the adresses of the I2C components on the network.
+#define PSoC0UnitAdress 0x08
+#define PSoC1UnitAdress 0x09
+#define PSoc2UnitAdress 0x10
+
+//Creates the buffer for the recieved data.
+uint8 recievedDataBuffer[2];
+uint8 transferErrorStatus;
 
 int main()
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    //Starts the I2C component on the PSoC.
+    I2C_1_Start();
+    I2C_1_I2CMasterClearStatus();
+    
     for(;;)
     {
-        /* Place your application code here. */
+        transferErrorStatus = I2C_1_I2CMasterSendStart(PSoC1UnitAdress, SCB_I2C_WRITE_XFER_MODE);
     }
 }
 
