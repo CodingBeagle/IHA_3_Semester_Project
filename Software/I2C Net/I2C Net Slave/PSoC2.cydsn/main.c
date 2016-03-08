23c8e8d17b246ff0c093a3f5855ead9ca4fb9385
CodingBeagle <kasper.rie@gmail.com>
2016-03-08 10:17:48 +0100

    Added I2C communication updates

--- Software/I2C Net/I2C Net Slave/PSoC2.cydsn/main.c
+++ Software/I2C Net/I2C Net Slave/PSoC2.cydsn/main.c
@@ -18,6 +18,11 @@
 #define PSoC1UnitAdress 0x09
 #define PSoc2UnitAdress 0x10
 
+//Defines the adresses of the I2C components on the network.
+#define PSoC0UnitAdress 0x08
+#define PSoC1UnitAdress 0x09
+#define PSoC2UnitAdress 0x10
+
 //Creates the buffer for the recieved data.
 uint8 recievedDataBuffer[2];
 uint8 transferErrorStatus;
@@ -37,18 +42,45 @@ int main()
     
     // Initialize Debug LED
     DebugLEDGREEN_Write(1);
-    DebugLEDRED_Write(0);
+    DebugLEDRED_Write(1);
+    
+    int isMaster = 0;
     
     // Main Loop
     for(;;)
     {
-        if (I2C_1_I2CSlaveStatus() == I2C_1_I2C_SSTAT_WR_CMPLT)
+        if (isMaster == 0)
         {
-            if ((int)buffer[0] == 12)
+            if (I2C_1_I2CSlaveStatus() == I2C_1_I2C_SSTAT_WR_CMPLT)
             {
-                DebugLEDGREEN_Write(0);
-                DebugLEDRED_Write(1);
+                if ((int)buffer[0] == 12)
+                {
+                    DebugLEDGREEN_Write(1);
+                    DebugLEDRED_Write(0);
+                    I2C_1_I2CSlaveClearReadStatus();
+                    isMaster = 1;
+                    I2C_1_I2CSlaveClearWriteBuf();
+                    CyDelay(1000);
+                }
             }
+        }
+        else
+        {
+           //I2C_1_I2CMasterClearStatus();
+           transferErrorStatus = I2C_1_I2CMasterSendStart(PSoC0UnitAdress, I2C_1_I2C_WRITE_XFER_MODE);
+            if(transferErrorStatus == I2C_1_I2C_MSTR_NO_ERROR)
+            {                
+                if (I2C_1_I2CMasterWriteByte(12) == I2C_1_I2C_MSTR_NO_ERROR)
+                {
+                    //Turn on Green LED while sending
+                    DebugLEDGREEN_Write(0);
+                    DebugLEDRED_Write(1);
+                    isMaster = 0;
+                }
+            }
+            I2C_1_I2CMasterSendStop();
+            I2C_1_I2CMasterClearStatus();
+            CyDelay(1000);
         }
     }
 }
